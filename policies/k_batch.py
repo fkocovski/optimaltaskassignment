221e4dace4acd4a3fa69350f7bfd6299953f7bd7
Filip Kocovski <filip.kocovski.90@gmail.com>
2017-01-20 09:03:24 +0100

    moved solver from parent signature to class signature

--- policies/k_batch.py
+++ policies/k_batch.py
@@ -14,8 +14,9 @@ Initializes a KBatch policy.
         :param file_policy: file object to calculate policy related statistics.
         :param file_statistics: file object to draw the policy evolution.
         """
-        super().__init__(env, number_of_users, worker_variability, solver, file_policy, file_statistics)
+        super().__init__(env, number_of_users, worker_variability, file_policy, file_statistics)
         self.batch_size = batch_size
+        self.solver = solver
         self.name = "{}Batch".format(self.batch_size)
         self.users_queues = [deque() for _ in range(self.number_of_users)]
         self.batch_queue = []
