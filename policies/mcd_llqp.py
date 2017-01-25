2da5756eaf9670977abd660844a5c91615d07aa0
Filip Kocovski <filip.kocovski.90@gmail.com>
2017-01-25 08:26:31 +0100

    adaptations for monte carlo llqp

--- policies/mcd_llqp.py
+++ policies/mcd_llqp.py
@@ -1,41 +1,28 @@
-from policies import *
+import numpy as np
 from collections import deque
+RANDOM_STATE = np.random.RandomState(1)
 
 
-class LLQP(Policy):
-    def __init__(self, env, number_of_users, worker_variability, file_policy, file_statistics):
-        """
-Initializes an LLQP policy.
-        :param env: simpy environment.
-        :param number_of_users: the number of users present in the system.
-        :param worker_variability: worker variability in absolute value.
-        :param file_policy: file object to calculate policy related statistics.
-        :param file_statistics: file object to draw the policy evolution.
-        """
-        super().__init__(env, number_of_users, worker_variability, file_policy, file_statistics)
+
+class LLQP(object):
+    def __init__(self, env, number_of_users, worker_variability, task_variability,service_interval,states_actions):
+        self.env = env
+        self.number_of_users = number_of_users
+        self.worker_variability = worker_variability
+        self.task_variability = task_variability
+        self.service_interval = service_interval
         self.name = "LLQP"
         self.users_queues = [deque() for _ in range(self.number_of_users)]
 
-        self.states_actions, self.states_actions_counts, self.returns = self.init_state_space_action_count()
-        self.rewards = []
-        self.history = []
-        self.mc_chunk = 0
-        self.current_state = None
+        self.states_actions = states_actions
 
-
-    def request(self, user_task):
-        """
-Request method for LLQP policies. Creates a PolicyJob object and calls for the appropriate evaluation method.
-        :param user_task: a user task object.
-        :return: a policyjob object to be yielded in the simpy environment.
-        """
-        super().request(user_task)
+    def request(self):
 
         average_processing_time = RANDOM_STATE.gamma(
-            user_task.service_interval ** 2 / user_task.task_variability,
-            user_task.task_variability / user_task.service_interval)
+            self.service_interval ** 2 / self.task_variability,
+            self.task_variability / self.service_interval)
 
-        llqp_job = PolicyJob(user_task)
+        llqp_job = PolicyJob()
         llqp_job.request_event = self.env.event()
         llqp_job.arrival = self.env.now
 
@@ -43,25 +30,18 @@ Request method for LLQP policies. Creates a PolicyJob object and calls for the a
                                                     self.worker_variability / average_processing_time) for
                                  _ in range(self.number_of_users)]
 
-        self.evaluate(llqp_job)
+        rwd,current_state = self.evaluate(llqp_job)
 
-        self.save_status()
 
-        return llqp_job,self.test
+        return llqp_job,rwd,current_state
 
     def release(self, llqp_job):
-        """
-Release method for LLQP policies. Uses the passed parameter, which is a policyjob previously yielded by the request method and releases it. Furthermore it frees the user that worked the passed policyjob object. If the released user's queue is not empty, it assigns the next policyjob to be worked.
-        :param llqp_job: a policyjob object.
-        """
-        super().release(llqp_job)
         user_to_release_index = llqp_job.assigned_user
 
         user_queue_to_free = self.users_queues[user_to_release_index]
 
         user_queue_to_free.popleft()
 
-        self.save_status()
 
         if len(user_queue_to_free) > 0:
             next_llqp_job = user_queue_to_free[0]
@@ -74,11 +54,7 @@ Evaluate method for LLQP policies. Looks for the currently least loaded person t
         :param llqp_job: a policyjob object to be assigned.
         """
 
-        if self.mc_chunk % 10 == 0 and self.mc_chunk != 0:
-            for action in range(2):
-                for a_one in range(100):
-                    for a_two in range(100):
-                        self.states_actions[action, a_one, a_two] += (1 /self.states_actions_counts[action, a_one, a_two]) * (self.returns[action, a_one, a_two] -self.states_actions[action, a_one, a_two])
+
 
         current_total_time = [None] * self.number_of_users
         for user_index, user_deq in enumerate(self.users_queues):
@@ -90,19 +66,20 @@ Evaluate method for LLQP policies. Looks for the currently least loaded person t
             else:
                 current_total_time[user_index] = 0
 
-        if numpy.random.random() < 0.1:
-            action = numpy.random.randint(0, self.number_of_users)
+        if np.random.random() < 0.1:
+            action = np.random.randint(0, 2)
         else:
-            a_one_max = numpy.amax(self.states_actions[0])
-            a_two_max = numpy.amax(self.states_actions[1])
+            a_one_max = np.amax(self.states_actions[:,:,0])
+            a_two_max = np.amax(self.states_actions[:,:,1])
             if a_one_max > a_two_max:
                 action = 0
             else:
                 action = 1
         a_one, a_two = self.get_current_state(current_total_time)
 
-        self.states_actions_counts[action, a_one, a_two] += 1
-        self.set_reward(action, a_one, a_two)
+        current_state = (a_one,a_two,action)
+
+        rwd = self.set_reward(action, a_one, a_two)
 
         llqp_queue = self.users_queues[action]
         llqp_job.assigned_user = action
@@ -112,44 +89,57 @@ Evaluate method for LLQP policies. Looks for the currently least loaded person t
             llqp_job.started = self.env.now
             llqp_job.request_event.succeed(llqp_job.service_rate[action])
 
-        self.mc_chunk += 1
-        self.test = 3
+        return rwd,current_state
 
-    def policy_status(self):
-        """
-Evaluates the current state of the policy. Overrides parent method with LLQP specific logic.
-        :return: returns a list where the first item is the global queue length (in LLQP always zero) and all subsequent elements are the respective user queues length.
-        """
-        current_status = [0]
-        for i in range(self.number_of_users):
-            current_status.append(len(self.users_queues[i]))
-        return current_status
-
-    def init_state_space_action_count(self):
-        states_actions = numpy.zeros((2, 100, 100))
-        states_actions_counts = numpy.ones((2, 100, 100), dtype=int)
-        returns = numpy.zeros((2, 100, 100))
-        return states_actions, states_actions_counts, returns
 
     def set_reward(self, action, a_one, a_two):
         if a_one < a_two and action == 0:
-            self.returns[action, a_one, a_two] = 1
+            return 1
         elif a_one > a_two and action == 1:
-            self.returns[action, a_one, a_two] = 1
+            return 1
         elif a_one == a_two:
-            self.returns[action, a_one, a_two] = 1
+            return 1
         else:
-            self.returns[action, a_one, a_two] = -10
+            return -1
 
     def get_current_state(self, current_waiting_times):
-        if current_waiting_times[0] is None:
-            a_one = 0
-        else:
-            a_one = int(current_waiting_times[0])
 
-        if current_waiting_times[1] is None:
-            a_two = 0
-        else:
-            a_two = int(current_waiting_times[1])
+        a_one = int(current_waiting_times[0])
+
+
+        a_two = int(current_waiting_times[1])
 
         return a_one, a_two
+
+class PolicyJob(object):
+    def __init__(self):
+        """
+Policy job object initialization method.
+        :param user_task: user task passed is used to uniquely identify it inside a workflow process.
+        """
+        self.arrival = None
+        self.started = None
+        self.finished = None
+        self.assigned_user = None
+        self.service_rate = None
+        self.request_event = None
+
+    def is_busy(self, now):
+        """
+Method used to determine whether a policy job object is currently being worked at the time passed.
+        :param now: current time passed as parameter.
+        :return: returns a boolean indicating whether the policy job object is busy or not.
+        """
+        if self.started is None:
+            return False
+        elif self.will_finish() < now:
+            return False
+        else:
+            return True
+
+    def will_finish(self):
+        """
+Method used to determine future finish time of a policy job object by adding its allocated service time of the user working it to its started time.
+        :return: returns a simpy time
+        """
+        return self.started + self.service_rate[self.assigned_user]
--- simulations/mcd_llqp_sim.py
+++ simulations/mcd_llqp_sim.py
@@ -1,41 +1,79 @@
 import simpy
-
-from evaluation.plot import evolution
-from elements.workflow_process_elements import StartEvent, UserTask, connect
-from evaluation.statistics import calculate_statistics
+import numpy as np
 from policies.mcd_llqp import LLQP
 from simulations import *
 
+RANDOM_STATE = np.random.RandomState(1)
 
-for i in range(2):
+states_actions = np.zeros((100, 100, 2))
+returns = []
+history = []
+
+class monte_carlo(object):
+    def __init__(self, env,states_actions):
+        self.env = env
+        self.policy = LLQP(self.env, NUMBER_OF_USERS, WORKER_VARAIBILITY, TASK_VARIABILITY, SERVICE_INTERVAL, states_actions)
+        self.action = env.process(self.generate_tokens())
+
+    def request_from_policy(self):
+        policy_job, rwd, current_state = self.policy.request()
+        returns.append(rwd)
+        history.append(current_state)
+        service_time = yield policy_job.request_event
+        yield self.env.timeout(service_time)
+        self.policy.release(policy_job)
+
+    def generate_tokens(self):
+        while True:
+            exp_arrival = RANDOM_STATE.exponential(GENERATION_INTERVAL)
+            yield self.env.timeout(exp_arrival)
+            self.env.process(self.request_from_policy())
+
+
+
+
+
+
+def update_q():
+    for index,sa in enumerate(history):
+        print(returns[index])
+        states_actions[sa] += (1 / history.count(sa)) * (returns[index] - states_actions[sa])
+
+for i in range(1):
     # creates simulation environment
     env = simpy.Environment()
 
     # open file and write header
-    file_policy,file_statistics,file_policy_name,file_statistics_name = create_files("mcd_llqp")
+    # file_policy, file_statistics, file_policy_name, file_statistics_name = create_files("mcd_llqp")
 
     # initialize policy
-    policy = LLQP(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, file_policy, file_statistics,test)
+    # policy = LLQP(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, TASK_VARIABILITY, SERVICE_INTERVAL,states_actions)
 
     # start event
-    start_event = StartEvent(env, GENERATION_INTERVAL)
+    # start_event = StartEvent(env, GENERATION_INTERVAL)
 
     # user tasks
-    user_task = UserTask(env, policy, "User task 1", SERVICE_INTERVAL, TASK_VARIABILITY)
+    # user_task = UserTask(env, policy, "User task 1", SERVICE_INTERVAL, TASK_VARIABILITY)
 
     # connections
-    connect(start_event, user_task)
+    # connect(start_event, user_task)
 
     # calls generation tokens process
-    env.process(start_event.generate_tokens())
+    # env.process(start_event.generate_tokens())
 
+    # env.process(generate_tokens(env))
+
+    mc = monte_carlo(env,states_actions)
     # runs simulation
     env.run(until=SIM_TIME)
-
+    update_q()
+    returns.clear()
+    history.clear()
     # close file
-    file_policy.close()
-    file_statistics.close()
+    # file_policy.close()
+    # file_statistics.close()
 
     # calculate statistics and plots
-    calculate_statistics(file_policy_name, outfile="{}.pdf".format(file_policy_name[:-4]))
-    evolution(file_statistics_name, outfile="{}.pdf".format(file_statistics_name[:-4]))
+    # calculate_statistics(file_policy_name, outfile="{}.pdf".format(file_policy_name[:-4]))
+    # evolution(file_statistics_name, outfile="{}.pdf".format(file_statistics_name[:-4]))
+
