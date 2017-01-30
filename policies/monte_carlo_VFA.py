2396e5b5b3d12f0c9e533b1a7f049705aaf53275
Filip Kocovski <filip.kocovski.90@gmail.com>
2017-01-30 13:50:55 +0100

    docstring for MC policy with VFA

--- policies/monte_carlo_VFA.py
+++ policies/monte_carlo_VFA.py
@@ -7,12 +7,16 @@ class MC(Policy):
     def __init__(self, env, number_of_users, worker_variability, file_policy, file_statistics, theta, epsilon, gamma,
                  alpha):
         """
-Initializes an LLQP policy.
+Initializes a MC policy with VFA.
         :param env: simpy environment.
         :param number_of_users: the number of users present in the system.
         :param worker_variability: worker variability in absolute value.
         :param file_policy: file object to calculate policy related statistics.
         :param file_statistics: file object to draw the policy evolution.
+        :param theta: weight vector for VFA.
+        :param epsilon: parameter for the epsilon greedy approach.
+        :param gamma: discounting factor for rewards.
+        :param alpha: step size parameter for the gradient descent method.
         """
         super().__init__(env, number_of_users, worker_variability, file_policy, file_statistics)
         self.name = "LLQP"
@@ -26,7 +30,7 @@ Initializes an LLQP policy.
 
     def request(self, user_task):
         """
-Request method for LLQP policies. Creates a PolicyJob object and calls for the appropriate evaluation method.
+Request method for MC policies. Creates a PolicyJob object and calls for the appropriate evaluation method.
         :param user_task: a user task object.
         :return: a policyjob object to be yielded in the simpy environment.
         """
@@ -56,7 +60,7 @@ Request method for LLQP policies. Creates a PolicyJob object and calls for the a
 
     def release(self, llqp_job):
         """
-Release method for LLQP policies. Uses the passed parameter, which is a policyjob previously yielded by the request method and releases it. Furthermore it frees the user that worked the passed policyjob object. If the released user's queue is not empty, it assigns the next policyjob to be worked.
+Release method for MC policies. Uses the passed parameter, which is a policyjob previously yielded by the request method and releases it. Furthermore it frees the user that worked the passed policyjob object. If the released user's queue is not empty, it assigns the next policyjob to be worked.
         :param llqp_job: a policyjob object.
         """
         super().release(llqp_job)
@@ -83,7 +87,7 @@ Release method for LLQP policies. Uses the passed parameter, which is a policyjo
 
     def evaluate(self, llqp_job):
         """
-Evaluate method for LLQP policies. Looks for the currently least loaded person to assign the policyjob.
+Evaluate method for MC policies. Creates a continuous state space which corresponds to the users busy times and follows and epsilon greedy policy approach to optimally choose the best user.
         :param llqp_job: a policyjob object to be assigned.
         """
         busy_times = [None] * self.number_of_users
@@ -116,8 +120,8 @@ Evaluate method for LLQP policies. Looks for the currently least loaded person t
 
     def policy_status(self):
         """
-Evaluates the current state of the policy. Overrides parent method with LLQP specific logic.
-        :return: returns a list where the first item is the global queue length (in LLQP always zero) and all subsequent elements are the respective user queues length.
+Evaluates the current state of the policy. Overrides parent method with MC specific logic.
+        :return: returns a list where the first item is the global queue length (in MC always zero) and all subsequent elements are the respective user queues length.
         """
         current_status = [0]
         for i in range(self.number_of_users):
@@ -125,34 +129,55 @@ Evaluates the current state of the policy. Overrides parent method with LLQP spe
         return current_status
 
     def action_value_approximator(self, states, action):
+        """
+Value function approximator. Uses the policy theta weight vector and returns for action and states vector an approximated value.
+        :param states: list of users busy time.
+        :param action: chosen action corresponding to the states.
+        :return: a single approximated value.
+        """
         value = 0.0
         for i, busy_time in enumerate(states):
             value += busy_time * self.theta[i + action * self.number_of_users]
         return value
 
     def update_theta(self):
+        """
+MC method to learn based on its followed trajectory. Evaluates the history list in reverse and for each states-action pair updates its internal theta vector.
+        """
         avg_lateness = np.average(self.jobs_lateness)
-        delta_theta = np.zeros(self.number_of_users ** 2)
         for i, (states, action, action_t) in enumerate(reversed(self.history)):
-            delta_theta = self.alpha * (
-            self.gamma ** i * -avg_lateness - self.action_value_approximator(states, action)) * self.gradient(states,
-                                                                                                              action)
-            self.theta += delta_theta
-        return delta_theta
+            self.theta += self.alpha * (
+                self.gamma ** i * -avg_lateness - self.action_value_approximator(states, action)) * self.gradient(
+                states,
+                action)
 
     def save_job_lateness(self, policy_job):
+        """
+Evaluates and appends the job's lateness to a policy global queue.
+        :param policy_job: policyjob object passed in each release method.
+        """
         job_lateness = policy_job.finished - policy_job.started
         self.jobs_lateness.append(job_lateness)
 
     def gradient(self, states, action):
+        """
+For each states-action pair calculates the gradient descent to be used in the theta update function.
+        :param states: list of users busy time.
+        :param action: chosen action corresponding to the states.
+        :return:
+        """
         gradient_vector = np.zeros(self.number_of_users ** 2)
         for i, busy_time in enumerate(states):
             gradient_vector[i + action * self.number_of_users] = busy_time
         return gradient_vector
 
     def value_function(self):
+        """
+Creates a list of approximated states-action values.
+        :return: list of approximated states-action values to be used as input for trisurf plot.
+        """
         value_action = []
-        for i,(states,action,_) in enumerate(self.history):
-            qsa_value = self.action_value_approximator(states,action)
-            value_action.append((states,qsa_value,action))
+        for i, (states, action, _) in enumerate(self.history):
+            qsa_value = self.action_value_approximator(states, action)
+            value_action.append((states, qsa_value, action))
         return value_action
