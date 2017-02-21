phi plot fix mc_vfa with fixed service times for users to lower variance

evaluation/phi_plot.py
@@ -5,13 +5,14 @@ import numpy as np
 def phi_plot(phis, rewards, outfile=None):
     avg_rewards = [np.mean(rwd) for rwd in rewards]
     std_rewards = [np.std(rwd) for rwd in rewards]
+    plt.figure(figsize=(2*len(rewards),len(rewards)))
     plt.xlabel(r"$\phi$ value")
     plt.ylabel("average reward")
     plt.grid(True)
     plt.errorbar(phis, avg_rewards, yerr=std_rewards, fmt="d", capsize=5, label="SD")
     plt.plot(phis, avg_rewards, label="Mean lateness")
-    plt.axvline(x=np.pi,c="r",ls="--",label=r"$\pi$")
-    plt.axvline(x=2*np.pi,c="g",ls="--",label=r"$2\pi$")
+    # plt.axvline(x=np.pi,c="r",ls="--",label=r"$\pi$")
+    # plt.axvline(x=2*np.pi,c="g",ls="--",label=r"$2\pi$")
     plt.legend()
 
     # output

policies/llqp/llqp_mc_vfa_fs.py
@@ -2,7 +2,7 @@ from policies import *
 from collections import deque
 
 
-class LLQP_MC_VFA_LR(Policy):
+class LLQP_MC_VFA_FS(Policy):
     def __init__(self, env, number_of_users, worker_variability, file_policy, file_statistics, theta, epsilon, gamma,
                  alpha):
         """
@@ -18,7 +18,7 @@ Initializes a MC policy with VFA.
         :param alpha: step size parameter for the gradient descent method.
         """
         super().__init__(env, number_of_users, worker_variability, file_policy, file_statistics)
-        self.name = "LLQP_MC_VFA_LR"
+        self.name = "LLQP_MC_VFA_FS"
         self.users_queues = [deque() for _ in range(self.number_of_users)]
         self.theta = theta
         self.epsilon = epsilon
@@ -33,7 +33,13 @@ Request method for MC policies. Creates a PolicyJob object and calls for the app
         :param user_task: a user task object.
         :return: a policyjob object to be yielded in the simpy environment.
         """
-        llqp_job = super().request(user_task)
+        # super().request(user_task)
+
+        llqp_job = PolicyJob(user_task)
+        llqp_job.request_event = self.env.event()
+        llqp_job.arrival = self.env.now
+
+        llqp_job.service_rate = [user_task.service_interval for _ in range(self.number_of_users)]
 
         self.save_status()
 
@@ -73,13 +79,11 @@ Evaluate method for MC policies. Creates a continuous state space which correspo
         :param llqp_job: a policyjob object to be assigned.
         """
         busy_times = self.get_busy_times()
-
         if RANDOM_STATE_ACTIONS.rand() < self.epsilon:
             action = RANDOM_STATE_ACTIONS.randint(0, self.number_of_users)
         else:
             action = max(range(self.number_of_users),
                          key=lambda action: self.q(busy_times, action))
-
 
         self.history.append((busy_times, action))
         self.rewards.append(busy_times[action] + llqp_job.service_rate[action])
@@ -132,18 +136,17 @@ Value function approximator. Uses the policy theta weight vector and returns for
 MC method to learn based on its followed trajectory. Evaluates the history list in reverse and for each states-action pair updates its internal theta vector.
         """
         for i, (states, action) in enumerate(self.history):
-            # if action > 0:
-                self.theta += self.alpha * (-self.discount_rewards(i) - self.q(states, action)) * self.features(states,action)
-                self.theta = self.normalize_theta()
+            self.theta += self.alpha * (self.discount_rewards(i) - self.q(states, action)) * self.features(states,action)
 
     def discount_rewards(self, time):
         """
 Discount rewards for one MC episode.
         """
         g = 0.0
-        for t in range(time + 1):
-            g += (self.gamma ** t) * self.rewards[t]
-        return g
+        tmp_rewards = self.rewards[time:]
+        for t,rwd in enumerate(tmp_rewards):
+            g += (self.gamma ** t) * rwd
+        return -g
 
     def features(self, states, action):
         """
@@ -162,4 +165,10 @@ Creates features vector for theta update function. For each action it creates a 
 Normalizes theta vector.
         :return: normalized theta vector.
         """
-        return self.theta/np.linalg.norm(self.theta)
+        self.theta /= np.linalg.norm(self.theta)
+
+    def compose_history(self):
+        composed_history = []
+        for i, (states, action) in enumerate(self.history):
+            composed_history.append((states,action,self.discount_rewards(i)))
+        return composed_history

simulations/llqp/llqp_mc_vfa_fs_phi_sim.py
@@ -4,7 +4,7 @@ import simpy
 from elements.workflow_process_elements import StartEvent, UserTask, connect
 from evaluation.plot import evolution
 from evaluation.statistics import calculate_statistics
-from policies.llqp.llqp_mc_vfa_lr import LLQP_MC_VFA_LR
+from policies.llqp.llqp_mc_vfa_fs import LLQP_MC_VFA_FS
 from simulations import *
 from evaluation.phi_plot import phi_plot
 
@@ -17,7 +17,7 @@ epsilon = 0.0
 phis = []
 list_of_rewards = []
 
-for phi in np.linspace(3/2*np.pi+1e-6, 2*np.pi, 32):
+for phi in np.linspace(4.5, 2*np.pi, 16):
     rewards = []
     for i in range(epochs):
         # creates simulation environment
@@ -30,7 +30,7 @@ for phi in np.linspace(3/2*np.pi+1e-6, 2*np.pi, 32):
         theta[2] = np.cos(phi)
         theta[3] = np.sin(phi)
 
-        policy_train = LLQP_MC_VFA_LR(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, None, None, theta/np.linalg.norm(theta), epsilon, gamma, alpha)
+        policy_train = LLQP_MC_VFA_FS(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, None, None, theta/np.linalg.norm(theta), epsilon, gamma, alpha)
 
         # start event
         start_event = StartEvent(env, GENERATION_INTERVAL)

simulations/llqp/llqp_mc_vfa_fs_sim.py
@@ -4,13 +4,13 @@ import simpy
 from elements.workflow_process_elements import StartEvent, UserTask, connect
 from evaluation.plot import evolution
 from evaluation.statistics import calculate_statistics
-from policies.llqp.llqp_mc_vfa_lr import LLQP_MC_VFA_LR
+from policies.llqp.llqp_mc_vfa_fs import LLQP_MC_VFA_FS
 from simulations import *
 
 # init theta and reinforcement learning variables
-theta = np.ones(NUMBER_OF_USERS ** 2)
+theta = np.zeros(NUMBER_OF_USERS ** 2)
 gamma = 0.9
-epochs = 1000
+epochs = 100
 alpha = 0.0001
 epsilon = 0.1
 
@@ -19,10 +19,7 @@ for i in range(epochs):
     env = simpy.Environment()
 
     # initialize policy
-    # FIXME: normalization as method
-    # theta[0] = 0.0
-    # theta[1] = 0.0
-    policy_train = LLQP_MC_VFA_LR(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, None, None, theta/np.linalg.norm(theta), epsilon, gamma, alpha)
+    policy_train = LLQP_MC_VFA_FS(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, None, None, theta, epsilon, gamma, alpha)
 
     # start event
     start_event = StartEvent(env, GENERATION_INTERVAL)
@@ -43,8 +40,9 @@ for i in range(epochs):
     env.run(until=SIM_TIME)
 
     # update theta
-    LLQP_MC_VFA_LR.update_theta(policy_train)
-    print(policy_train.theta)
+    LLQP_MC_VFA_FS.update_theta(policy_train)
+    if i % 500 == 0:
+        print("Finished {}th train run".format(i))
 
 # set epsilon to 0.0 to make test policy behave full greedy
 epsilon = 0.0
@@ -53,10 +51,10 @@ epsilon = 0.0
 env = simpy.Environment()
 
 # open file and write header
-file_policy, file_statistics, file_policy_name, file_statistics_name = create_files("LLQP_MC_VFA_LR")
+file_policy, file_statistics, file_policy_name, file_statistics_name = create_files("LLQP_MC_VFA_FS")
 
 # initialize policy
-policy = LLQP_MC_VFA_LR(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, file_policy, file_statistics, theta, epsilon, gamma,
+policy = LLQP_MC_VFA_FS(env, NUMBER_OF_USERS, WORKER_VARAIBILITY, file_policy, file_statistics, theta, epsilon, gamma,
                      alpha)
 
 # start event
@@ -73,6 +71,8 @@ env.process(start_event_test.generate_tokens())
 
 # runs simulation
 env.run(until=SIM_TIME)
+
+print(policy.compose_history())
 
 # close file
 file_policy.close()