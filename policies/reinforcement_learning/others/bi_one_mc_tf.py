b62a27060939197b0b09b3470f0eac1f3f78cb9b
Filip Kocovski <filip.kocovski.90@gmail.com>
2017-03-27 14:45:45 +0200

    changed to new project interpreter (renamed old conda env) fixes on bi one mc tf policy

--- policies/reinforcement_learning/others/bi_one_mc_tf.py
+++ policies/reinforcement_learning/others/bi_one_mc_tf.py
@@ -5,7 +5,7 @@ from policies import *
 from datetime import datetime
 
 
-class BI_ONE_TD_TF(Policy):
+class BI_ONE_MC_TF(Policy):
     def __init__(self, env, number_of_users, worker_variability, file_policy, gamma,
                  greedy, wait_size, sess, weights, biases, n_input):
         super().__init__(env, number_of_users, worker_variability, file_policy)
@@ -14,17 +14,16 @@ class BI_ONE_TD_TF(Policy):
         self.wait_size = wait_size
         self.sess = sess
         self.RANDOM_STATE_ACTIONS = pcg.RandomState(1)
-        self.name = "BI_ONE_TD_TF"
+        self.name = "BI_ONE_MC_TF"
         self.user_slot = [None] * self.number_of_users
         self.batch_queue = []
         self.history = []
         self.rewards = []
-        self.count = 0
 
         with tf.name_scope("input"):
             self.inp = tf.placeholder(tf.float32, name="state_space", shape=(1, n_input))
-            self.gradient_input = tf.placeholder(tf.float32,name="gradient_input",shape=(self.number_of_users,1))
-            self.factor_input = tf.placeholder(tf.float32,name="factor_input")
+            self.gradient_input = tf.placeholder(tf.float32, name="gradient_input", shape=(self.number_of_users, 1))
+            self.factor_input = tf.placeholder(tf.float32, name="factor_input")
 
         with tf.name_scope("neural_network"):
             layer_1 = tf.add(tf.matmul(self.inp, weights['h1']), biases['b1'])
@@ -33,13 +32,14 @@ class BI_ONE_TD_TF(Policy):
             layer_2 = tf.nn.relu(layer_2)
             self.pred = [tf.matmul(layer_2, weights['out'][b]) + biases['out'][b] for b in range(self.wait_size)]
             self.probabilities = [tf.nn.softmax(self.pred[b]) for b in range(self.wait_size)]
-            self.cost = [tf.matmul(self.probabilities[b],self.gradient_input) for b in range(self.wait_size)]
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-            self.gradients = [self.optimizer.compute_gradients(self.cost[b],[weights["h1"],weights["h2"],weights["out"][b],biases["b1"],biases["b2"],biases["out"][b]]) for b in range(self.wait_size)]
-            self.apply = [[] for _ in range(self.wait_size)]
-            for b in range(self.wait_size):
-                for g,v in self.gradients[b]:
-                    self.apply[b].append((g*self.factor_input,v))
+            self.cost = [tf.matmul(self.probabilities[b], self.gradient_input) for b in range(self.wait_size)]
+            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+            self.gradients = [self.optimizer.compute_gradients(self.cost[b],
+                                                               [weights["h1"], weights["h2"], weights["out"][b],
+                                                                biases["b1"], biases["b2"], biases["out"][b]]) for b in
+                              range(self.wait_size)]
+            self.apply = [self.optimizer.apply_gradients([(g * self.factor_input, v) for g, v in self.gradients[b]]) for
+                          b in range(self.wait_size)]
 
         tf_init = tf.global_variables_initializer()
         self.sess.run(tf_init)
@@ -90,10 +90,6 @@ class BI_ONE_TD_TF(Policy):
         self.rewards.append(self.reward(w, p, a, choices))
         self.history.append((state, output, choices))
 
-        # if self.count % 10 == 0:
-        #     self.discount_rewards(self.count)
-        # self.count += 1
-
     def state_space(self):
         # wj
         w = [self.env.now - self.batch_queue[j].arrival for j in range(self.wait_size)]
@@ -112,35 +108,17 @@ class BI_ONE_TD_TF(Policy):
 
         return state, w, p, a
 
-
     def update_theta(self):
-        for i, (state,output,choices) in enumerate(self.history):
+        for i, (state, output, choices) in enumerate(self.history):
             rewards = self.discount_rewards(i)
-            for index,preferences in enumerate(output):
-                reshape_output = np.reshape(preferences,(self.number_of_users,1))
+            for index, preferences in enumerate(output):
+                reshape_output = np.reshape(preferences, (self.number_of_users, 1))
                 chosen_user = choices[index]
                 prob_value = output[index][0][chosen_user]
                 reward = rewards[index]
-                # for operation in range(len(self.gradients[index])):
-                # TODO: maybe pro job input
-                grads_vals = self.sess.run(self.gradients[index],{self.inp:state,self.gradient_input:reshape_output})
-                factor = reward/prob_value
-                # np.multiply(grads_vals[0], factor, grads_vals[0])
-                # grads = grads_vals[0]
-                # vals = grads_vals[1]
-
-                # for g,v in grads_vals:
-                #     np.multiply(g,factor,g)
-
-                # print(grads_vals)
-
-                # new_grads_vals = []
-                # for var_grad,var_val in zip(grads,vals):
-                #     new_grads_vals.append((var_grad*factor,var_val))
-
-
-                self.sess.run(self.apply[index],{self.inp:state,self.gradient_input:reshape_output,self.factor_input:factor})
-
+                factor = reward / prob_value
+                self.sess.run(self.apply[index],
+                              {self.inp: state, self.gradient_input: reshape_output, self.factor_input: factor})
 
     def reward(self, w, p, a, choices):
         reward = []
@@ -151,8 +129,8 @@ class BI_ONE_TD_TF(Policy):
         return reward
 
     def discount_rewards(self, time):
-        g = [0.0]*self.wait_size
+        g = [0.0] * self.wait_size
         for t in range(time + 1):
-            for reward_index,reward in enumerate(self.rewards[t]):
+            for reward_index, reward in enumerate(self.rewards[t]):
                 g[reward_index] += (self.gamma ** t) * reward
         return g
--- simulations/reinforcement_learning/others/bi_one_mc_tf_sim.py
+++ simulations/reinforcement_learning/others/bi_one_mc_tf_sim.py
@@ -2,37 +2,37 @@ import simpy
 import tensorflow as tf
 from evaluation.subplot_evolution import evolution
 from evaluation.statistics import calculate_statistics
-from policies.reinforcement_learning.others.bi_one_td_tf import BI_ONE_TD_TF
+from policies.reinforcement_learning.others.bi_one_mc_tf import BI_ONE_MC_TF
 from simulations import *
 
-policy_name = "{}_BI_ONE_TD_TF_NU{}_GI{}_TRSD{}_SIM{}".format(BATCH_SIZE, NUMBER_OF_USERS, GENERATION_INTERVAL, SEED,
+policy_name = "{}_BI_ONE_MC_TF_NU{}_GI{}_TRSD{}_SIM{}".format(BATCH_SIZE, NUMBER_OF_USERS, GENERATION_INTERVAL, SEED,
                                                               SIM_TIME)
 n_input = BATCH_SIZE + NUMBER_OF_USERS * BATCH_SIZE + NUMBER_OF_USERS  # wj+pij+ai
 n_out = NUMBER_OF_USERS
 n_hidden_1 = n_input * 10
 n_hidden_2 = n_input * 10
-epochs = 20
+epochs = 5
 
 with tf.name_scope("weights"):
     weights = {
-        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1]), name="h1"),
-        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name="h2"),
-        'out': [tf.Variable(tf.random_normal([n_hidden_2, n_out]), name="out") for _ in range(BATCH_SIZE)]
+        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1],seed=SEED), name="h1"),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2],seed=SEED+1), name="h2"),
+        'out': [tf.Variable(tf.random_normal([n_hidden_2, n_out],seed=SEED+2), name="out") for _ in range(BATCH_SIZE)]
     }
 with tf.name_scope("biases"):
     biases = {
-        'b1': tf.Variable(tf.random_normal([n_hidden_1]), name="b1"),
-        'b2': tf.Variable(tf.random_normal([n_hidden_2]), name="b2"),
-        'out': [tf.Variable(tf.random_normal([n_out]), name="out") for _ in range(BATCH_SIZE)]
+        'b1': tf.Variable(tf.random_normal([n_hidden_1],seed=SEED+3), name="b1"),
+        'b2': tf.Variable(tf.random_normal([n_hidden_2],seed=SEED+4), name="b2"),
+        'out': [tf.Variable(tf.random_normal([n_out],seed=SEED+5), name="out") for _ in range(BATCH_SIZE)]
     }
 
 with tf.Session() as sess:
     gamma = 0.5
     sim_time_training = SIM_TIME * 2
-    for _ in range(epochs):
+    for i in range(epochs):
         env = simpy.Environment()
 
-        policy_train = BI_ONE_TD_TF(env, NUMBER_OF_USERS, WORKER_VARIABILITY, None, gamma, False, BATCH_SIZE, sess, weights,
+        policy_train = BI_ONE_MC_TF(env, NUMBER_OF_USERS, WORKER_VARIABILITY, None, gamma, False, BATCH_SIZE, sess, weights,
                                     biases, n_input)
 
         start_event = acquisition_process(env, policy_train, SEED, GENERATION_INTERVAL, False, None, None, None)
@@ -41,12 +41,13 @@ with tf.Session() as sess:
 
         env.run(until=sim_time_training)
         policy_train.update_theta()
+        print("finished {}".format(i))
 
     env = simpy.Environment()
 
     file_policy = create_files("{}.csv".format(policy_name))
 
-    policy = BI_ONE_TD_TF(env, NUMBER_OF_USERS, WORKER_VARIABILITY, file_policy, gamma, True, BATCH_SIZE, sess, weights,
+    policy = BI_ONE_MC_TF(env, NUMBER_OF_USERS, WORKER_VARIABILITY, file_policy, gamma, True, BATCH_SIZE, sess, weights,
                           biases, n_input)
 
     start_event = acquisition_process(env, policy, 1, GENERATION_INTERVAL, False, None, None, None)
